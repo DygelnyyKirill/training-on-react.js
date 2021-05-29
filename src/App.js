470f1e4f7f460eab747010ef8e3deedb2050d7fd
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2021-05-29 16:16:45 +0300

    this.props

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import TodoList from './Todo/TodoList';
+import Header from './Header';
+
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    {id: 1, completed: false, title: 'Go to work'},
+    {id: 2, completed: true, title: 'Go to park'},
+    {id: 3, completed: false, title: 'Go to the cinima'},
+  ]);
+
+  const menu = [
+    {
+      link: '/articles',
+      label: 'Articles'
+    },
+    {
+      link: '/contacts',
+      label: 'Contacts'
+    },
+    {
+      link: '/posts',
+      label: 'Posts'
+    }
+  ];
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  return (
+  <div className="wrapper">
+    <Header items={menu} />
+    <h1>React tutorial</h1>
+    <TodoList todos={todos} onToggle={toggleTodo}/>
+  </div>
+  );  
+}
+
+export default App;
